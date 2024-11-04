81ff6de848fc639174a342fc9506f905798ac2c9
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-11-04 15:17:07 +0100

    feat: add axios + firebase backend

--- screens/ManageExpenses.js
+++ screens/ManageExpenses.js
@@ -2,9 +2,9 @@ import { useContext, useLayoutEffect } from "react";
 import { StyleSheet, View } from "react-native";
 import IconButton from "../components/UI/IconButton";
 import { GlobalStyles } from "../constants/styles";
-import Button from "../components/UI/Button";
 import { ExpensesContext } from "../store/expenses-context";
 import ExpenseForm from "../components/ManageExpense/ExpenseForm";
+import { storeExpense } from "../util/http";
 
 export default function ManageExpenses({route, navigation}){
     const expensesCtx = useContext(ExpensesContext);
@@ -33,6 +33,7 @@ export default function ManageExpenses({route, navigation}){
         if (isEditing) {
             expensesCtx.updateExpense(editedExpenseId, expenseData);
         } else {
+            storeExpense(expenseData);
             expensesCtx.addExpense(expenseData);
         }
         navigation.goBack();
--- store/expenses-context.js
+++ store/expenses-context.js
@@ -1,71 +1,9 @@
 import { createContext, useReducer } from "react";
 
-const DUMMY_EXPENSES = [
-    {
-        id: 'e1',
-        description: 'A pair of shoes',
-        amount: 59.99,
-        date: new Date('2024-12-19'),
-    },
-    {
-        id: 'e2',
-        description: 'A pair of trousers',
-        amount: 89.29,
-        date: new Date('2024-01-05'),
-    },
-    {
-        id: 'e3',
-        description: 'Some bananas',
-        amount: 5.99,
-        date: new Date('2024-12-01'),
-    },
-    {
-        id: 'e4',
-        description: 'A book',
-        amount: 14.99,
-        date: new Date('2024-02-19'),
-    },
-    {
-        id: 'e5',
-        description: 'Another book',
-        amount: 18.59,
-        date: new Date('2024-02-18'),
-    },
-    {
-        id: 'e6',
-        description: 'A pair of shoes',
-        amount: 59.99,
-        date: new Date('2024-12-19'),
-    },
-    {
-        id: 'e7',
-        description: 'A pair of trousers',
-        amount: 89.29,
-        date: new Date('2024-01-05'),
-    },
-    {
-        id: 'e8',
-        description: 'Some bananas',
-        amount: 5.99,
-        date: new Date('2024-12-01'),
-    },
-    {
-        id: 'e9',
-        description: 'A book',
-        amount: 14.99,
-        date: new Date('2024-02-19'),
-    },
-    {
-        id: 'e10',
-        description: 'Another book',
-        amount: 18.59,
-        date: new Date('2024-02-18'),
-    },
-]
-
 export const ExpensesContext = createContext({
     expenses: [],
     addExpense: ({description, amount, date}) => {},
+    setExpenses: (expenses) => {},
     deleteExpense: (id) => {},
     updateExpense: (id, {description, amount, date}) => {},
 });
@@ -74,7 +12,9 @@ function expensesReducer(state, action){
     switch (action.type) {
         case 'ADD':
             const id = new Date().toString() + Math.random().toString();
-            return [{...action.payload, id: id}, ...state]
+            return [{...action.payload, id: id}, ...state];
+        case 'SET':
+            return action.payload;
         case 'UPDATE':
             const updatableExpenseIndex = state.findIndex((expense) => expense.id === action.payload.id);
             const updatableExpense = state[updatableExpenseIndex];
@@ -90,10 +30,14 @@ function expensesReducer(state, action){
 }
 
 export default function ExpensesContextProvider({children}) {
-    const [expensesState, dispatch] = useReducer(expensesReducer, DUMMY_EXPENSES);
+    const [expensesState, dispatch] = useReducer(expensesReducer, []);
 
     function addExpense(expenseData) {
         dispatch({ type: 'ADD', payload: expenseData});
+    }
+
+    function setExpenses(expenses){
+        dispatch({type: 'SET', payload: expenses});
     }
 
     function deleteExpense(id) {
@@ -107,6 +51,7 @@ export default function ExpensesContextProvider({children}) {
     const value = {
         expenses: expensesState,
         addExpense: addExpense,
+        setExpenses: setExpenses,
         deleteExpense: deleteExpense,
         updateExpense: updateExpense,
     };
